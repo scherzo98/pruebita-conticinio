e3e0cb45cf1599c595f3f047f22bbef24e0cde59
SirFateAndRage <milanicristian94@gmail.com>
2021-10-11 11:30:07 -0300

    visual

---
+++ Conticinio/Assets/Scripts/PlatForm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatForm : MonoBehaviour
+{
+    [SerializeField] Transform pos1, pos2;
+    [SerializeField] float _speed;
+    [SerializeField] Transform _starPos;
+
+    Vector3 nextPos;
+
+    private void Start()
+    {
+        nextPos = _starPos.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if(transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, _speed * Time.deltaTime);
+    }
+}
